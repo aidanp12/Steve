a6076d54c318c925452190fb4035b49ef67f2ffc
SioneDaniels <142916691+SioneDaniels@users.noreply.github.com>
2025-04-05 15:14:54 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,2 +1,9 @@
-class player(magic, melee, ranged, weapon, armor, health, mana):
-  
+class player
+  def __init__(magic, melee, ranged, weapon, armor, health, mana):
+    self.magic = magic
+    self.melee = melee
+    self.ranged = ranged
+    self.weapon = weapon
+    self.armor = armor
+    self.health = health
+    self.mana = mana
