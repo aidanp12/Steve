677659e195f3eaa1425ebcf2ab42c8984c427077
MinoMax0205 <143829310+MinoMax0205@users.noreply.github.com>
2025-04-05 15:04:59 -0500

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -1,8 +1,8 @@
 class Weapon:
     def __init__(self, ranged, dmg_type, dmg, enchanted, dur, pierce):
-        self._ranged = ranged
-        self._dmg_type = dmg_type
-        self._dmg = dmg
-        self._enchanted = enchanted
-        self._dur = dur
-        self._pierce = pierce
+        self.ranged = ranged
+        self.dmg_type = dmg_type
+        self.dmg = dmg
+        self.enchanted = enchanted
+        self.dur = dur
+        self.pierce = pierce
