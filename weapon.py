988f42cecd6f344382321c089619f3b0e475ccbb
MinoMax0205 <143829310+MinoMax0205@users.noreply.github.com>
2025-04-05 14:11:37 -0500

    weapon class to store traits

---
+++ weapon.py
@@ -0,0 +1,8 @@
+class Weapon:
+    def __init__(self, ranged, dmg_type, dmg, enchanted, dur, pierce):
+        self._ranged = ranged
+        self._dmg_type = dmg_type
+        self._dmg = dmg
+        self._enchanted = enchanted
+        self._dur = dur
+        self._pierce = pierce
