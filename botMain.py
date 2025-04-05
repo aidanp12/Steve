706bcfb76622d454c263bc853ff4fa2f5f896243
SioneDaniels <142916691+SioneDaniels@users.noreply.github.com>
2025-04-05 18:37:18 -0500

    Update botMain.py

--- botMain.py
+++ botMain.py
@@ -70,11 +70,11 @@ class BotMain:
 
         @self.bot.command()
         async def inventory(ctx):
-            await ctx.send(self.users[ctx.author][playerData(view_inventory())])
+            await ctx.send(self.users[ctx.author][playerData.view_inventory()])
 
         @self.bot.command()
-        async def equip(ctx):
-            pass
+        async def equip(ctx, parameter):
+           await ctx.send(self.users[ctx.author][playerData.view_inventory()])
 
     def run(self):
         # Run the bot with the token
