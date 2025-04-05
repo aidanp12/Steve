07801bb9b3f08abcdecc6fc2fe94cc7f151496c5
aidanp12 <aidanp1230@icloud.com>
2025-04-05 13:51:02 -0500

    0.1.0
    Basic bot functionality

--- botMain.py
+++ botMain.py
@@ -8,3 +8,28 @@ Contributors:
 """
 
 import discord
+from discord.ext import commands
+
+class botMain:
+    def __init__(self):
+        self.intents = discord.Intents.default()
+        self.intents.message_content = True
+        self.initialize()
+
+    def initialize(self):
+        bot = commands.Bot(command_prefix="!", intents=self.intents)
+
+        # Event when the bot has successfully connected
+        @bot.event
+        async def on_ready():
+            print(f"Bot connected as {bot.user}")
+
+        # Command example: !hello
+        @bot.command()
+        async def hello(ctx):
+            await ctx.send("Flint and Steeeel!")
+
+        # Run the bot with the token
+        bot.run("MTM1Nzk1NjE4MzY2MjUyNjY1MQ.Gx4MPy.NVgjMoS2MEDLx8Qt5pn-Drmgdv0yEKsInvLEDU")
+
+
