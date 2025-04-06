f54c10afdce3e04c1b5a53ae603fdbf9202b2f8a
MinoMax0205 <143829310+MinoMax0205@users.noreply.github.com>
2025-04-05 20:20:23 -0500

    Update Mob.py

--- Mob.py
+++ Mob.py
@@ -1,5 +1,9 @@
 class Mob:
-    def __init__(self, hp, dmg):
+    def __init__(self, name, hp, dmg):
+        self.name = name
         self.max_hp = hp
         self.cur_hp = hp
         self.dmg = dmg
+
+    def take_dmg(self, dmg):
+        self.cur_hp -= dmg:
