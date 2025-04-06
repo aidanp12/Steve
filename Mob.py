27cdb032de7fa50c2b56c1211ddde684a3add6e2
MinoMax0205 <143829310+MinoMax0205@users.noreply.github.com>
2025-04-05 20:28:00 -0500

    Update Mob.py
    Added self.alive to determine if enemy is still alive

--- Mob.py
+++ Mob.py
@@ -4,6 +4,9 @@ class Mob:
         self.max_hp = hp
         self.cur_hp = hp
         self.dmg = dmg
-
+        self.alive = True
+        
     def take_dmg(self, dmg):
         self.cur_hp -= dmg:
+        if self.cur_hp < 0:
+            self.alive = False
