b04cdc7b057efd9c5ef70eac04637c406a38d290
SioneDaniels <142916691+SioneDaniels@users.noreply.github.com>
2025-04-06 02:27:06 -0500

    Update roll.py

--- roll.py
+++ roll.py
@@ -7,21 +7,21 @@ def diceRoll(input):
     results = []
     for i in range(rolls):
         match dice:
-            case "d2":
+            case 2:
                 results.append(random.randint(1, 2))
-            case "d4":
+            case 4:
                 results.append(random.randint(1, 4))
-            case "d8":
-                results.append(random.randint(1, 8))
-            case "d6":
+            case 6:
                 results.append(random.randint(1, 6))
-            case "d10":
+            case 8:
+                results.append(random.randint(1, 8))
+            case 10:
                 results.append(random.randint(1, 10))
-            case "d20":
+            case 20:
                 results.append(random.randint(1, 20))
-            case "d50":
+            case 50:
                 results.append(random.randint(1, 50))
-            case "d100":
+            case 100:
                 results.append(random.randint(1, 100))
             case _:
                 return 0
