70abaeb2bebaedaa45191a0077758990a0a0e6f7
SioneDaniels <142916691+SioneDaniels@users.noreply.github.com>
2025-04-05 21:35:10 -0500

    Update Mob.py

--- Mob.py
+++ Mob.py
@@ -1,10 +1,11 @@
 class Mob:
-    def __init__(self, name, hp, dmg):
+    def __init__(self, name, hp, dmg, ambush):
         self.name = name
         self.max_hp = hp
         self.cur_hp = hp
         self.dmg = dmg
         self.alive = True
+        self.ambush = ambush
         
     def take_dmg(self, dmg):
         self.cur_hp -= dmg:
