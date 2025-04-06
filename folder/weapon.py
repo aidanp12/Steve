b8ceb149f83121273975f333123c4d858efc5baf
SioneDaniels <142916691+SioneDaniels@users.noreply.github.com>
2025-04-06 02:05:41 -0500

    Update weapon.py

--- folder/weapon.py
+++ folder/weapon.py
@@ -1,5 +1,6 @@
 class Weapon:
-    def __init__(self, dmg_type, dmg, rarity, dur, pierce):
+    def __init__(self, name, dmg_type, dmg, rarity, dur, pierce):
+        self.name = name
         self.dmg_type = dmg_type
         self.dmg = dmg
         self.rarity = rarity
